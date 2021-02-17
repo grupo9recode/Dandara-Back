db560d787a4f55c03a8ad1989e29af375597a202
Leonardo <novagenisissistemas@gmail.com>
2021-02-17 15:50:54 -0300

    crud quase completo

--- app/App.js
+++ app/App.js
@@ -1,27 +1,54 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const Produto = require('./models/Produto')
-const handlebars = require('express-handlebars');
-const cors = require('cors');
-//const { post } = require('./Config/Server');
-const app = express();
-const admin = require("./routes/admin")
+//Carregando Modulos
+    const express = require('express')
+    const bodyParser = require('body-parser')
+    const Produto = require('./models/Produto')
+    const handlebars = require('express-handlebars');
+    const cors = require('cors');
+    const mongoose = require("mongoose");
+    //const { post } = require('./Config/Server');
+    const app = express();
+    const admin = require("./routes/admin")
+    const session = require('express-session')
+    const flash = require("connect-flash")
 
 
 //Configurações
+    //Sessão
+    app.use(session({
+        secret: "dandara",
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+    //Middleware
+    app.use((req,res, next) => {
+        res.locals.success_msg = req.flash("success_msg")
+        res.locals.error_msg = req.flash("error_msg")
+        next()
+    })
+
     //Handlebars
-        app.engine('handlebars', handlebars({defaultLayout:'main'}))
-        app.set('view engine', 'handlebars')
+    app.engine('handlebars', handlebars({defaultLayout:'main'}))
+    app.set('view engine', 'handlebars')
     //Body-parser
     app.use(bodyParser.urlencoded({extended:false}))
     app.use(bodyParser.json())
     //Cors
     app.use(cors())
+    //Mongoose
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/dandara').then(() =>{
+        console.log("MongoDb Conectado a Dandara")
+    }).catch((err) =>{
+        console.log("Erro ao se conectar ao Mongo: " +err)
+    })
+
+
 
 //Rotas
     
     //rota adminitrativa
-    app.use('admin',admin)
+    app.use('/admin',admin)
 
     //retorno dos dados em Json
     app.get('/', async (req,res) => {
@@ -60,6 +87,8 @@ const admin = require("./routes/admin")
     })
     
     })
+
+
 
 //informação da porta do servidor
 const PORT = 3050
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,20 +1,74 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/categorias')
+const Categoria = mongoose.model('categorias')
+
 
 router.get('/', (req,res) => {
-
+    res.render("admin/index")
 })
 
 router.get('/produtos', (req,res) => {
-    
+    res.send('Pagina de produtos')
 })
 
-router.get('/categorias', (req,res) => {
-    res.render('admin/categorias')
+router.get('/categorias', async (req,res) => {
+    const categoriasResponse = await Categoria.find()
+    const categoriasJson = await categoriasResponse
+    
+    return res.json(categoriasJson)
+    
 })
 
 router.get('/categorias/add', (req,res) => {
     res.render("admin/addcategorias")
 })
+
+//rota para criar categorias
+router.post('/categorias/nova', (req,res) => {
+   
+   /*
+   validação de formulário
+   var erros = []
+
+   if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido"})
+   }
+
+   if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "slug inválido"})
+    }
+   */
+
+
+    const novaCategoria ={
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(() => {
+        console.log("Categoria cadastrada com sucesso")
+    }).catch((err) => {
+        console.log("Erro ao cadastrar a nova categoria")
+    })
+    
+
+})
+
+
+//editar categoria
+router.put("/categorias/:id", async(req,res) =>{
+    const {id} = req.params;
+    const categoria = await Categoria.findOne({_id: id});
+
+    categoria.nome = req.body.nome;
+    categoria.slug = req.body.slug;
+
+    categoria.save();
+
+    res.json({message: "categoria editada"})
+})
+
 
 module.exports = router
